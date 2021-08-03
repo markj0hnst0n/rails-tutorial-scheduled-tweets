bf799ce44b038b77f9c98e89ad5696bfccee680a
mark.j0hnst0n <friendorfaux6@gmail.com>
2021-08-03 12:42:10 +0100

    Homepage and links wired up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: "main#index"
+  get "about-us", to: "about#index", as: :about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
