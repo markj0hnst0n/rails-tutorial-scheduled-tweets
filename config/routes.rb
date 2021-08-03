5a741442bba3de323788e97431a6d9030fac10f1
mark.j0hnst0n <friendorfaux6@gmail.com>
2021-08-03 17:03:51 +0100

    loutgout functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "about-us", to: "about#index", as: :about
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "session#destroy"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
