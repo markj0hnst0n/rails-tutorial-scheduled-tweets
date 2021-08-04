42f81284981103d19ca1e4660db0fef2a9b255f6
mark.j0hnst0n <friendorfaux6@gmail.com>
2021-08-04 12:56:23 +0100

    added password reset funtionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "main#index"
+
   get "about-us", to: "about#index", as: :about
 
   get "password", to: "passwords#edit", as: :edit_password
@@ -12,6 +13,12 @@ Rails.application.routes.draw do
   post "sign_in", to: "session#create"
   
   delete "logout", to: "session#destroy"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
