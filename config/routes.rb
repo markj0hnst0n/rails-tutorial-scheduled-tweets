e7c37079543cb7b27ab053ee22cbd1da39084926
mark.j0hnst0n <friendorfaux6@gmail.com>
2021-08-04 15:05:22 +0100

    twitter api connected and omniauth used to connect to twitter account

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,11 @@ Rails.application.routes.draw do
 
   get "password/reset", to: "password_resets#new"
   post "password/reset", to: "password_resets#create"
-  
+
   get "password/reset/edit", to: "password_resets#edit"
   patch "password/reset/edit", to: "password_resets#update"
+
+  get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
