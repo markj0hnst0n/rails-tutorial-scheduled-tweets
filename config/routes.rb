7bf29d793598d48e9ef0911b24c50e72d81a2190
mark.j0hnst0n <friendorfaux6@gmail.com>
2021-08-04 09:38:24 +0100

    user login functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root to: "main#index"
   get "about-us", to: "about#index", as: :about
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "session#new"
+  post "sign_in", to: "session#create"
+  
   delete "logout", to: "session#destroy"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
