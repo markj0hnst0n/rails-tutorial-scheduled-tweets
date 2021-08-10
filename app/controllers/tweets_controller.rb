8611894d7e7345f6aaf684985a1425c0c80f8a17
mark.j0hnst0n <friendorfaux6@gmail.com>
2021-08-10 12:56:58 +0100

    edit and delete scheduled tweet functionality added

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
 class TweetsController < ApplicationController
 	before_action :require_user_logged_in!
+	before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@tweets = Current.user.tweets
 	end
@@ -16,10 +18,30 @@ class TweetsController < ApplicationController
 			render :new
 		end
 	end
+
+	def edit
+	end
+
+	def update
+		if @tweet.update(tweet_params)
+			redirect_to tweets_path, notice: "Tweet was updated successfully"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@tweet.destroy
+		redirect_to tweets_path, notice: "Tweet was unscheduled"
+	end
 	
 	private
 
 	def tweet_params
 		params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+	end
+
+	def set_tweet
+		@tweet = Current.user.tweets.find(params[:id])
 	end
 end
